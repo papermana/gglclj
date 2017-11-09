2f4b6a08446438fed76f0cf3c9de586770d13dea
Dominik Rowicki <dominik.rowicki@netguru.co>
2017-11-10 00:35:13 +0100

    Add first working version of the app

--- src/gglclj/core.cljs
+++ src/gglclj/core.cljs
@@ -1,9 +1,57 @@
 (ns gglclj.core
-  (:require [clojure.browser.repl :as repl]))
+  (:require [clojure.string :as string]
+            [cljs.nodejs :as node]))
 
-;; (defonce conn
-;;   (repl/connect "http://localhost:9000/repl"))
+(def os (node/require "os"))
+(def child_process (node/require "child_process"))
 
-(enable-console-print!)
+(node/enable-util-print!)
 
-(println "Hello world!")
+(defn get-search-func
+  [engine]
+  (case engine
+    :images #(str "https://www.google.com/search?q="
+                  (string/join "%20" %)
+                  "&tbm=isch")
+    :youtube #(str "https://www.youtube.com/results?search_query=\""
+                   (string/join "%20" %)
+                   "\"")
+    :stackoverflow #(str "http://stackoverflow.com/search?q="
+                         (string/join "+" %))))
+
+(defn get-search-engine
+  [flag]
+  (case flag
+    ("-i" "--images") :images
+    ("-y" "--youtube") :youtube
+    ("-s" "--stack" "--stackoverflow") :stackoverflow))
+
+(defn open
+  [func query]
+  (let [command (if (= (.platform os) "darwin")
+                  "open"
+                  "xdg-open")]
+    (.spawn child_process command #js[(func query)])))
+
+(defn print-help!
+  []
+  (println "Pass a flag and then a query"))
+
+(defn help-flag?
+  [flag]
+  (or (= flag "-h")
+      (= flag "--help")
+      (= flag "-?")))
+
+(defn -main
+  [flag & query]
+  (cond (and (not flag) (not query)) (print-help!)
+        (help-flag? flag) (print-help!)
+        :else (-> flag
+                  get-search-engine
+                  get-search-func
+                  (open query))))
+
+(set! *main-cli-fn* -main)
+
+
